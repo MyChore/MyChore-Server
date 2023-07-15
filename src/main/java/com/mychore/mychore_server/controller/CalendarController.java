eb19cdbe255756e9868fd371411940ce06b023f6
psyeon1120 <psycsh@naver.com>
2023-07-16 02:33:40 +0900

    #1 feat: 컨트롤러 내 어노테이션 추가 및 테스트용 컨트롤러 생성

--- src/main/java/com/mychore/mychore_server/controller/CalendarController.java
+++ src/main/java/com/mychore/mychore_server/controller/CalendarController.java
@@ -1,7 +1,11 @@
 package com.mychore.mychore_server.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "/calendars")
+@RequiredArgsConstructor
 public class CalendarController {
 }
