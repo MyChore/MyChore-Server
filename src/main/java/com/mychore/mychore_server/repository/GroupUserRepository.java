8c46f9086667034238ab070973d21269856f887e
Taeyoung Kim <lucy_ia@ewhain.net>
2023-08-07 21:05:22 +0900

    #10 refactor: validation check에 owner check 추가

--- src/main/java/com/mychore/mychore_server/repository/GroupUserRepository.java
+++ src/main/java/com/mychore/mychore_server/repository/GroupUserRepository.java
@@ -3,6 +3,7 @@ package com.mychore.mychore_server.repository;
 import com.mychore.mychore_server.entity.group.Group;
 import com.mychore.mychore_server.entity.group.GroupUser;
 import com.mychore.mychore_server.entity.user.User;
+import com.mychore.mychore_server.global.constants.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -13,7 +14,7 @@ public interface GroupUserRepository extends JpaRepository<GroupUser, Long> {
     Optional<GroupUser> findByUserAndGroupAndStatus(User user, Group group, String status);
     List<GroupUser> findGroupUsersByGroupAndStatus(Group group, String status);
     List<GroupUser> findByUserAndStatus(User user, String status);
-
+    Optional<GroupUser> findByUserAndRoleAndStatus(User user, Role role, String status);
     Optional<GroupUser> findGroupUserByUserAndGroupAndStatus(User user, Group group, String status);
 
 
--- src/main/java/com/mychore/mychore_server/service/GroupService.java
+++ src/main/java/com/mychore/mychore_server/service/GroupService.java
@@ -195,8 +195,23 @@ public class GroupService {
         return resDTO;
     }
 
+    private Group groupOwnerCheck(Long groupId, Long userId){
+//        validation check
+        Group group = groupRepository.findById(groupId)
+                .orElseThrow(() -> new BaseException(BaseResponseCode.NOT_FOUND_GROUP));
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BaseException(BaseResponseCode.NOT_FOUND_USER));
+        groupUserRepository.findByUserAndGroupAndStatus(user, group, ACTIVE_STATUS)
+                .orElseThrow(() -> new BaseException(BaseResponseCode.NO_PERMISSION));
+
+//        owner check
+        groupUserRepository.findByUserAndRoleAndStatus(user, OWNER, ACTIVE_STATUS)
+                .orElseThrow(() -> new BaseException(BaseResponseCode.NO_PERMISSION));
+
+        return group;
+    }
     public StaticDataResDTO updateGroupName(Long groupId, String newName, Long userId){
-        Group group = validationCheck(groupId, userId);
+        Group group = groupOwnerCheck(groupId, userId);
         group.SetName(newName);
         return getStaticData(groupId, userId);
     }
