961437abc89a0b4cf60c7f4667041b427b61f700
psyeon1120 <psycsh@naver.com>
2023-08-09 03:48:57 +0900

    #23 feat: 프로필 이미지 삭제 API

--- src/main/java/com/mychore/mychore_server/controller/UserController.java
+++ src/main/java/com/mychore/mychore_server/controller/UserController.java
@@ -28,20 +28,20 @@ public class UserController {
 
     // 회원가입
     @PostMapping
-    public BaseResponse<UserTokenRes> signUp(@RequestBody @Valid UserSignUpReq userSignUpReq){
+    public BaseResponse<UserTokenRes> signUp(@RequestBody @Valid UserSignUpReq userSignUpReq) {
         return new BaseResponse<>(userService.signUp(userSignUpReq));
     }
 
     // 로그인
     @PostMapping("/login")
-    public BaseResponse<UserTokenRes> logIn(@RequestBody @Valid UserLogInReq userLogInReq){
+    public BaseResponse<UserTokenRes> logIn(@RequestBody @Valid UserLogInReq userLogInReq) {
         return new BaseResponse<>(userService.login(userLogInReq));
     }
 
     // 로그아웃
     @Auth
     @PatchMapping("/logout")
-    public BaseResponse<Void> logOut(@IsLogin LoginStatus loginStatus){
+    public BaseResponse<Void> logOut(@IsLogin LoginStatus loginStatus) {
         userService.logout(loginStatus.getUserId());
         return new BaseResponse<>(BaseResponseCode.SUCCESS);
     }
@@ -49,7 +49,7 @@ public class UserController {
     // 회원탈퇴
     @Auth
     @DeleteMapping
-    public BaseResponse<Void> withdraw(@IsLogin LoginStatus loginStatus){
+    public BaseResponse<Void> withdraw(@IsLogin LoginStatus loginStatus) {
         userService.withdraw(loginStatus.getUserId());
         return new BaseResponse<>(BaseResponseCode.SUCCESS);
     }
@@ -57,7 +57,7 @@ public class UserController {
     // 프로필 조회
     @Auth
     @GetMapping
-    public BaseResponse<GetProfileRes> getProfile(@IsLogin LoginStatus loginStatus){
+    public BaseResponse<GetProfileRes> getProfile(@IsLogin LoginStatus loginStatus) {
         return new BaseResponse<>(userService.getProfile(loginStatus.getUserId()));
     }
 
@@ -65,7 +65,7 @@ public class UserController {
     @Auth
     @PatchMapping
     public BaseResponse<Void> editProfile(@RequestBody @Valid PatchProfileReq patchProfileReq,
-                                          @IsLogin LoginStatus loginStatus){
+                                          @IsLogin LoginStatus loginStatus) {
         userService.editProfile(loginStatus.getUserId(), patchProfileReq);
         return new BaseResponse<>(BaseResponseCode.SUCCESS);
     }
@@ -74,23 +74,31 @@ public class UserController {
     @Auth
     @PatchMapping("profileimg")
     public BaseResponse<Void> editProfileImg(@RequestBody Map<String, String> imgUrl,
-                                             @IsLogin LoginStatus loginStatus){
+                                             @IsLogin LoginStatus loginStatus) {
         if (imgUrl.get(IMG_URL) == null) return new BaseResponse<>(BaseResponseCode.NULL_IMG_URL);
         userService.editProfileImg(loginStatus.getUserId(), imgUrl.get(IMG_URL));
+        return new BaseResponse<>(BaseResponseCode.SUCCESS);
+    }
+
+    // 프로필 사진 삭제
+    @Auth
+    @DeleteMapping("profileimg")
+    public BaseResponse<Void> deleteProfileImg(@IsLogin LoginStatus loginStatus) {
+        userService.editProfileImg(loginStatus.getUserId(), null);
         return new BaseResponse<>(BaseResponseCode.SUCCESS);
     }
 
     // 알림 설정 수정
     @Auth
     @PatchMapping("/noti")
-    public BaseResponse<Void> editNotiAgree(@RequestParam String type, @IsLogin LoginStatus loginStatus){
+    public BaseResponse<Void> editNotiAgree(@RequestParam String type, @IsLogin LoginStatus loginStatus) {
         userService.editNotiAgree(loginStatus.getUserId(), type);
         return new BaseResponse<>(BaseResponseCode.SUCCESS);
     }
 
     // 가입 전 닉네임 중복 확인
     @GetMapping("check-nickname")
-    public BaseResponse<Boolean> checkNickname(@RequestParam String nickname){
+    public BaseResponse<Boolean> checkNickname(@RequestParam String nickname) {
         return new BaseResponse<>(userService.checkNicknameWithSignUp(nickname));
     }
 }
