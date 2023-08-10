0b1c3b01f9b5cbd0351330ea6d75de16a6734b16
633jinn <judyg00dy68@gmail.com>
2023-08-10 15:03:02 +0900

    #11 feat: Notification toEntity 추가, RequestDTO추가

---
+++ src/main/java/com/mychore/mychore_server/dto/notification/request/NotiChoreReq.java
@@ -0,0 +1,14 @@
+package com.mychore.mychore_server.dto.notification.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class NotiChoreReq { // 그룹원 집안일 완료 알림
+    private Long groupId;
+    private Long choreId;
+    private String deviceToken;
+}
