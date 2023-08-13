d6853f41ae8829a0e33e525e5397357b9fa66938
633jinn <judyg00dy68@gmail.com>
2023-08-13 18:09:23 +0900

    #11 feat: 에러 메세지 삭제

--- src/main/java/com/mychore/mychore_server/global/exception/BaseResponseCode.java
+++ src/main/java/com/mychore/mychore_server/global/exception/BaseResponseCode.java
@@ -21,8 +21,6 @@ public enum BaseResponseCode {
     // token
     AUTH_ANNOTATION_IS_NOWHERE("T0001", HttpStatus.UNAUTHORIZED, "토큰을 통해 userId를 추출하는 메서드에는 @Auth 어노테이션을 붙여주세요."),
     EXPIRED_TOKEN("T0002", HttpStatus.FORBIDDEN, "만료된 토큰 값입니다."),
-    INVALID_DEVICE_TOKEN("T0003",HttpStatus.BAD_REQUEST, "잘못된 디바이스 토큰값입니다."),
-    NOT_FOUND_GROUP_DEVICE_TOKEN("T0004", HttpStatus.NOT_FOUND,"그룹안의 알림을 보낼 수 있는 디바이스토큰이 없습니다."),
 
     // user
     NOT_FOUND_USER("U0001", HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
@@ -38,7 +36,6 @@ public enum BaseResponseCode {
     NULL_PROVIDER("U0011", HttpStatus.BAD_REQUEST, "Provider를 입력해 주세요."),
     NULL_IS_14OVER("U0012", HttpStatus.BAD_REQUEST, "만 14세 이상 동의 여부를 입력해주세요."),
     NULL_IS_Accept_EMAIL("U0013", HttpStatus.BAD_REQUEST, "이메일 수신 동의 여부를 입력해주세요."),
-    NOT_ACCEPT_NOTI("U0014",HttpStatus.BAD_REQUEST,"사용자가 알림을 허용하지 않았습니다."),
     NULL_IMG_URL("U0014", HttpStatus.BAD_REQUEST, "이미지 url이 없습니다."),
 
     // group
@@ -62,10 +59,7 @@ public enum BaseResponseCode {
     NULL_CHORE_NAME("C0007", HttpStatus.BAD_REQUEST, "집안일 이름을 입력해주세요."),
     NULL_START_DATE("C0008", HttpStatus.BAD_REQUEST, "시작날짜를 입력해주세요."),
     NULL_IS_ACCEPT_NOTI("C0009", HttpStatus.BAD_REQUEST, "알림 수신여부를 입력해주세요."),
-    ALREADY_COMPLETE_CHORE("C0010", HttpStatus.CONFLICT,"이미 완료한 집안일입니다."),
-    NOT_COMPLETE_CHORE("C0011", HttpStatus.CONFLICT, "완료되지 않은 집안일 입니다."),
-    NOT_FOUND_CHORE_LOG("C0012", HttpStatus.NOT_FOUND, "집안일 로그를 찾을 수 없습니다."),
-
+    NOT_FOUND_CHORE_LOG("C0010", HttpStatus.NOT_FOUND, "집안일 로그를 찾을 수 없습니다."),
     ;
 
     public final String code;
